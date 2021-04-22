df2c1b4c6bed3e34bdab9ab2f8c8fdcc27a1f0d6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 12:56:09 +0300

    Rokas

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
+
 
 # Create your views here.
 
-
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
