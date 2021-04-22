3af0bd868de737d19c19895a678c5f5cf2b8a1d6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 12:17:53 +0300

    Rokas

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Project(models.Model):
+
+    name = models.CharField(max_length=200)
+    language = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+    website = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
